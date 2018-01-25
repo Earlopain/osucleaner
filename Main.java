5e71ee38e5c78c9052db1ec42f7fa1451c071057
Earlopain <j.scholz41@gmx.de>
2018-01-25 03:13:59 +0100

    Moved config move function to Util

--- Main.java
+++ Main.java
@@ -11,31 +11,8 @@ public class Main {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        moveOldConfigs();
         GUI window = new GUI();
-    }
-
-    public static void moveOldConfigs() {
-
-        File[] folderFiles = new File("./").listFiles();
-        String[] logFiles = new String[folderFiles.length];
-        boolean logFileAlreadyExists = false;
-        int counter = 0;
-        for (File file : folderFiles) {
-            if (file.isFile()) {
-                if (file.getName().startsWith(Options.logFile.substring(2))) {
-                    logFiles[counter] = (file.getAbsolutePath());
-                    counter++;
-                }
-                if (file.getName().equals(Options.logFile.substring(2)))
-                    logFileAlreadyExists = true;
-            }
-        }
-        if (logFileAlreadyExists) {
-            //start with the last one, so we don't overwrite files
-            for (int i = counter - 1; i >= 0; i--) {
-                new File(logFiles[i]).renameTo(new File(Options.logFile + (i + 1)));
-            }
-        }
+        Util.moveOldConfigs();
+        Logger.start();
     }
 }
