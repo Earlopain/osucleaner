791dfe71f1169fcd34e58084543eac236fa84b1a
Earlopain <j.scholz41@gmx.de>
2018-01-25 23:23:35 +0100

    Logger writes progress every 1 second to disk

--- Logger.java
+++ Logger.java
@@ -1,23 +1,40 @@
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Logger {
+    private static ScheduledExecutorService executor;
     private static Writer w;
-    static {
+
+    static{
         try {
             w = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(Options.logFile), "utf-8"));
         } catch (Exception e) {
-            System.out.println(e);
+            Util.throwException(new Exception("Logfile could not be created"));
         }
+        executor = Executors.newScheduledThreadPool(1);
+        Runnable writeLog = new Runnable() {
+            public void run() {
+                try {
+                    w.flush();
+                } catch (Exception e) {
+                    Util.notice("Logfile moved or deleted");
+                }
+            }
+        };
+        executor.scheduleAtFixedRate(writeLog, 0, 1, TimeUnit.SECONDS);
     }
 
     public static void log(String text) {
         try {
             w.write("\r\n" + text);
         } catch (Exception e) {
-            System.out.println(e);
         }
     }
 
@@ -29,18 +46,15 @@ public class Logger {
             }
             w.write(whitespace + text);
         } catch (Exception e) {
-            System.out.println(e);
         }
     }
 
     public static void end() {
+        executor.shutdown();
         try {
             w.close();
         } catch (Exception e) {
-            System.out.println(e);
+            Util.notice("Logfile could not be saved");
         }
-    }
-    //called to init static block
-    public static void start(){
     }
 }
