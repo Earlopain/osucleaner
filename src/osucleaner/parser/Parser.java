1bb09d2ac663547726c87fa0e49a17702adac239
Earlopain <earlopain@c5h8no4na.net>
2018-02-10 15:03:09 +0100

    Removed old parsing logic

--- src/osucleaner/parser/Parser.java
+++ src/osucleaner/parser/Parser.java
@@ -14,46 +14,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Parser {
-    public static String get(File file, String property, String findThis, boolean regex) throws FileNotFoundException {
-        boolean inProperty = false;
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"))) {
-            String line;
-
-            while ((line = br.readLine()) != null) {
-                if (inProperty) {
-                    if (line.startsWith("[")) {
-                        br.close();
-                        return null;
-                    }
-
-                    if (regex) {
-                        Pattern p = Pattern.compile(findThis);
-                        Matcher m = p.matcher(line);
-                        while (m.find()) {
-                            br.close();
-                            return m.group(0);
-                        }
-                    } else {
-                        final String[] splitted = line.split(":", 2);
-                        if (splitted[0].equals(findThis)) {
-                            br.close();
-                            return splitted[1].startsWith(" ") ? splitted[1].substring(1) : splitted[1];
-                        }
-                    }
-                }
-                if (line.startsWith("[" + property))
-                    inProperty = true;
-
-            }
-            //probably the filepaths are too long, or file was deleted, but believe it's the other
-        } catch (FileNotFoundException e) {
-            throw e;
-        } catch (Exception e) {
-            Util.throwException(e);
-        }
-        return null;
-    }
-
     public static Results get(File file, Parameter paras, String... optional) throws FileNotFoundException {
         String encoding = (optional.length == 0) ? "UTF-8": optional[0];
         try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), encoding))) {
